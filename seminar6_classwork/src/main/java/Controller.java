74e054fed6dac7d232a82dd5d1b253bb89661574
Nikita Kuznetsov <nkuz1509@gmail.com>
2023-03-24 18:37:11 +0300

    add hw_6

---
+++ seminar6_classwork/src/main/java/Controller.java
@@ -0,0 +1,20 @@
+public class Controller {
+    private View view;
+    private Model model;
+
+    public Controller(View view, Model model) {
+        this.view = view;
+        this.model = model;
+    }
+
+    public void login(String password){
+        String result = "Неверный пароль.";
+
+        if(model.getPassword().equals(password)){
+            result = "Правильный пароль!";
+            view.hideInputField();
+        }
+
+        view.updateStatusLabel(result);
+    }
+}
---
+++ seminar6_classwork/src/main/java/Model.java
@@ -0,0 +1,15 @@
+public class Model {
+    private String password;
+
+    public Model(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
